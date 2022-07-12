9fb90478913e5e693185cf1369b845a46c7e56eb
lllrdgz <lllrdgz.cs@gmail.com>
2022-07-11 21:25:00 -0400

    Add report

--- src/Generator/Random.hs
+++ src/Generator/Random.hs
@@ -4,15 +4,3 @@ import System.Random
 
 getRandom :: Int -> Int -> IO Int
 getRandom x y = getStdRandom $ randomR (x,y)
-
--- drawDouble :: Double -> Double -> IO Double
--- drawDouble x y = getStdRandom (randomR (x,y))
-
--- handler :: Int -> Int -> IO ()
--- handler min max = do
---     int <- getRandom min max 
---     let out = useInt int
---     writeFile ".seed" (show out) 
-
--- useInt :: Int -> Int
--- useInt x = x
